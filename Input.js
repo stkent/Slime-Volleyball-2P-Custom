9828b2a5a866db3f507d5b36282084955d826472
Stuart Kent <skent.dev@gmail.com>
2016-08-10 20:47:35 +0100

    Clean up input handling code

--- Input.js
+++ Input.js
@@ -1,13 +1,10 @@
-var KEY_A = 65;
-var KEY_D = 68;
-var KEY_W = 87;
-
 var KEY_SPACE = 32;
-
 var KEY_LEFT = 37;
 var KEY_UP = 38;
 var KEY_RIGHT = 39;
-
+var KEY_A = 65;
+var KEY_D = 68;
+var KEY_W = 87;
 
 var keysDown = {};
 
