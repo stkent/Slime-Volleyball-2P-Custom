af584a7c51c7b9a3f4c23d8de7faf470b041f30c
Stuart Kent <skent.dev@gmail.com>
2016-08-10 20:46:20 +0100

    Suppress warnings in input handling code

--- Input.js
+++ Input.js
@@ -5,11 +5,13 @@ var KEY_W = 87;
 var KEY_SPACE = 32;
 
 var KEY_LEFT = 37;
-var KEY_UP = 38
-var KEY_RIGHT = 39
+var KEY_UP = 38;
+var KEY_RIGHT = 39;
 
 
 var keysDown = {};
+
+//noinspection SpellCheckingInspection
 addEventListener("keydown", function (e) {
     //console.log("keydown '" + e.keyCode + "'");
     if (e.keyCode == KEY_SPACE) {
@@ -18,6 +20,8 @@ addEventListener("keydown", function (e) {
         keysDown[e.keyCode] = true;
     }
 }, false);
+
+//noinspection SpellCheckingInspection
 addEventListener("keyup", function (e) {
     //console.log("keyup   '" + e.keyCode + "'");
     keysDown[e.keyCode] = false;
